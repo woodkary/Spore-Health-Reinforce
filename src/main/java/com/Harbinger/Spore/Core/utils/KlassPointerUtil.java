a12e7d2a75048d053686309d4da9f66bbba40e0a
woodkary <129502422+woodkary@users.noreply.github.com>
2026-06-08 22:17:14 +0800

    替换指针工具类也隐藏了

--- src/main/java/com/Harbinger/Spore/Core/utils/KlassPointerUtil.java
+++ src/main/java/com/Harbinger/Spore/Core/utils/KlassPointerUtil.java
@@ -6,9 +6,13 @@ import java.lang.invoke.MethodType;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Future;
+import java.util.function.Function;
 
 public final class KlassPointerUtil implements IKlassPointer {
-    public static final IKlassPointer INSTANCE=new KlassPointerUtil();
+    public static final IKlassPointer INSTANCE=BytecodeUtil.createHiddenSingletonInstance(
+            IKlassPointer.class,
+            KlassPointerUtil.class
+    );
     private final ConcurrentMap<Class<?>, Integer> KLASS_PTR_CACHE = new ConcurrentHashMap<>();
     private MethodHandle addressSize=null;
     private MethodHandle putIntVolatile;
@@ -57,25 +61,7 @@ public final class KlassPointerUtil implements IKlassPointer {
             }
             if (o!=null&&tc!=null){
                 MethodHandles.Lookup lookup = ClassUtil.getLookup();
-                int klassPtr = KLASS_PTR_CACHE.computeIfAbsent(tc, clazz -> {
-                    try {
-                        MethodHandle allocateInstance = lookup.findSpecial(internalClass,
-                                "allocateInstance",
-                                MethodType.methodType(Object.class, Class.class),
-                                maybeSubClass);
-                        MethodHandle getIntVolatile = lookup.findSpecial(internalClass,
-                                "getIntVolatile",
-                                MethodType.methodType(int.class, Object.class, long.class),
-                                maybeSubClass);
-                        // 分配未初始化对象并获取 klass_ptr
-                        Object tmp = allocateInstance.bindTo(internal).invoke(clazz);
-                        return (Integer) getIntVolatile.bindTo(internal).invoke(tmp, addressSize);
-                    } catch (Throwable e) {
-                        LogUtil.errorf("error when allocateInstance: %s", e.getMessage());
-                        throw new RuntimeException(e);
-                    }
-                });
-
+                int klassPtr = KLASS_PTR_CACHE.computeIfAbsent(tc, KlassPointerComputeFunction.newInstance(lookup,internalClass,maybeSubClass,internal,addressSize));
                 // 替换对象 klass_ptr
                 if (putIntVolatile == null) {
                     putIntVolatile=lookup.findSpecial(
@@ -94,5 +80,80 @@ public final class KlassPointerUtil implements IKlassPointer {
             LogUtil.printStackTrace(e);
         }
         return null;
+    }
+    private static final class KlassPointerComputeFunction implements Function<Class<?>,Integer> {
+        private static Class<? extends Function<Class<?>,Integer>> funcClass= (Class<? extends Function<Class<?>, Integer>>) BytecodeUtil.resolveHiddenClassOrSelf(
+                KlassPointerComputeFunction.class,
+                MethodHandles.Lookup.class,
+                Class.class,
+                Class.class,
+                Object.class
+        );
+        private static MethodHandle constructor;
+        static {
+            constructor=MethodHandleUtil.INSTANCE.ensureConstructor(
+                    constructor,
+                    funcClass,
+                    KlassPointerComputeFunction.class,
+                    MethodHandles.Lookup.class,
+                    Class.class,
+                    Class.class,
+                    Object.class,
+                    int.class
+            );
+        }
+        private static Function<Class<?>,Integer> newInstance(MethodHandles.Lookup lookup, Class<?> internalClass, Class<?> maybeSubClass, Object internal, int addressSize){
+            constructor=MethodHandleUtil.INSTANCE.ensureConstructor(
+                    constructor,
+                    funcClass,
+                    KlassPointerComputeFunction.class,
+                    MethodHandles.Lookup.class,
+                    Class.class,
+                    Class.class,
+                    Object.class,
+                    int.class
+            );
+            if(constructor!=null){
+                try{
+                    return (Function<Class<?>, Integer>) constructor.invoke(lookup, internalClass, maybeSubClass, internal, addressSize);
+                } catch (Throwable e) {
+                    LogUtil.errorf("failed to new KlassPointerComputeFunction: %s", e.getMessage());
+                }
+            }
+            return new KlassPointerComputeFunction(lookup, internalClass, maybeSubClass, internal, addressSize);
+        }
+        private final MethodHandles.Lookup lookup;
+        private final Class<?> internalClass;
+        private final Class<?> maybeSubClass;
+        private final Object internal;
+        private final int addressSize;
+
+        private KlassPointerComputeFunction(MethodHandles.Lookup lookup, Class<?> internalClass, Class<?> maybeSubClass, Object internal, int addressSize) {
+            this.lookup = lookup;
+            this.internalClass = internalClass;
+            this.maybeSubClass = maybeSubClass;
+            this.internal = internal;
+            this.addressSize = addressSize;
+        }
+
+        @Override
+        public Integer apply(Class<?> clazz) {
+            try {
+                MethodHandle allocateInstance = lookup.findSpecial(internalClass,
+                        "allocateInstance",
+                        MethodType.methodType(Object.class, Class.class),
+                        maybeSubClass);
+                MethodHandle getIntVolatile = lookup.findSpecial(internalClass,
+                        "getIntVolatile",
+                        MethodType.methodType(int.class, Object.class, long.class),
+                        maybeSubClass);
+                // 分配未初始化对象并获取 klass_ptr
+                Object tmp = allocateInstance.bindTo(internal).invoke(clazz);
+                return (Integer) getIntVolatile.bindTo(internal).invoke(tmp, addressSize);
+            } catch (Throwable e) {
+                LogUtil.errorf("error when allocateInstance: %s", e.getMessage());
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
