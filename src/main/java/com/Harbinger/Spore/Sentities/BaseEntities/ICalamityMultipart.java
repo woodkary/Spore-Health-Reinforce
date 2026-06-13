5b253897fc76a9674536b495304f626ed6367700
woodkary <129502422+woodkary@users.noreply.github.com>
2026-06-13 09:40:40 +0800

    准备设计LeviathanMultipart

--- src/main/java/com/Harbinger/Spore/Sentities/BaseEntities/ICalamityMultipart.java
+++ src/main/java/com/Harbinger/Spore/Sentities/BaseEntities/ICalamityMultipart.java
@@ -3,5 +3,5 @@ package com.Harbinger.Spore.Sentities.BaseEntities;
 import net.minecraft.world.entity.LivingEntity;
 
 public interface ICalamityMultipart {
-    LivingEntity getCalamityHead();
+    Calamity getCalamityHead();
 }
