9a445caef95a66757524174cb6c8ece2c6fbbc7a
woodkary <129502422+woodkary@users.noreply.github.com>
2026-07-09 13:18:52 +0800

    补充LocalPlayer清除客户端药水效果显示

--- src/main/java/com/Harbinger/Spore/mixin/LocalPlayerMixin.java
+++ src/main/java/com/Harbinger/Spore/mixin/LocalPlayerMixin.java
@@ -1,12 +1,16 @@
 package com.Harbinger.Spore.mixin;
 
 import com.Harbinger.Spore.Core.asmHooks.EntityHeealuthManager;
+import com.Harbinger.Spore.Core.utils.unremovableCollections.ISporeMap;
 import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectInstance;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LocalPlayer.class)
 public class LocalPlayerMixin {
@@ -15,5 +19,12 @@ public class LocalPlayerMixin {
     @Inject(method = "respawn",at=@At("HEAD"))
     public void removeDeafgthTags(CallbackInfo ci){
         EntityHeealuthManager.INSTANCE.setPlayerAlliive(spore$localPlayer);
+    }
+    @Inject(method="removeEffectNoUpdate",at=@At("RETURN"))
+    public void removeISporeMapEffectNoUpdate(MobEffect effect, CallbackInfoReturnable<MobEffectInstance> cir){
+        if(!(spore$localPlayer.activeEffects instanceof ISporeMap<MobEffect, MobEffectInstance> sporeMap)){
+            return;
+        }
+        sporeMap.actualRemove(effect);
     }
 }
