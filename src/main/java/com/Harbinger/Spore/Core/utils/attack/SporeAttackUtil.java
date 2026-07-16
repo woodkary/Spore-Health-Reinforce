6531e16940c041c9b45dddbabb157a4807453630
woodkary <129502422+woodkary@users.noreply.github.com>
2026-07-16 15:49:56 +0800

    尝试检查无敌并清除

--- src/main/java/com/Harbinger/Spore/Core/utils/attack/SporeAttackUtil.java
+++ src/main/java/com/Harbinger/Spore/Core/utils/attack/SporeAttackUtil.java
@@ -279,7 +279,7 @@ public final class SporeAttackUtil implements IAttack {
             }
         }
         EntityHeealuthManager.INSTANCE.killEntity(target,damageSource);
-        if(target.getHealth()<=0&&attacker instanceof Calamity&&!(target instanceof Player)){
+        if(target.getHealth()<=0.0f&&attacker instanceof Calamity&&!(target instanceof Player)){
             //清除赖着不死的实体
             EntityInvulCheckTaskManager.INSTANCE.add(target);
         }
