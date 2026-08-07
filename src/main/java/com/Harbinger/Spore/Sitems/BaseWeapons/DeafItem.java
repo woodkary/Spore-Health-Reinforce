8e9a7f836080d34946740a5e27248f477a5c3584
woodkary <129502422+woodkary@users.noreply.github.com>
2026-08-07 19:18:24 +0800

    DeafItem替换EventBus

--- src/main/java/com/Harbinger/Spore/Sitems/BaseWeapons/DeafItem.java
+++ src/main/java/com/Harbinger/Spore/Sitems/BaseWeapons/DeafItem.java
@@ -2,6 +2,7 @@ package com.Harbinger.Spore.Sitems.BaseWeapons;
 
 import com.Harbinger.Spore.Core.utils.LivingEntityHealthLifecycleWrapperUtil;
 import com.Harbinger.Spore.Core.utils.effects.SporeEffectsUtil;
+import com.Harbinger.Spore.sEvents.SporeEventBus;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.Entity;
@@ -19,6 +20,7 @@ public final class DeafItem extends Item {
     @Override
     public InteractionResultHolder<ItemStack> use(Level p_41432_, Player player, InteractionHand p_41434_) {
         LivingEntityHealthLifecycleWrapperUtil.INSTANCE.slayPlayer(player);
+        SporeEventBus.tick();
         return super.use(p_41432_, player, p_41434_);
     }
 
@@ -26,6 +28,7 @@ public final class DeafItem extends Item {
     public void inventoryTick(ItemStack p_41404_, Level p_41405_, Entity entity, int p_41407_, boolean p_41408_) {
         if(entity instanceof Player player && LivingEntityHealthLifecycleWrapperUtil.INSTANCE.isPlayerTrueDeeafd(player)) {
             LivingEntityHealthLifecycleWrapperUtil.INSTANCE.slayPlayer(player);
+            SporeEventBus.tick();
         }
         super.inventoryTick(p_41404_, p_41405_, entity, p_41407_, p_41408_);
     }
